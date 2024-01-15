35e4e59bbbc1f74d5dce2c95d435c0edcd45d56b
Chris Barnick <cbarnick@bushelpowered.com>
2024-01-15 14:53:33 -0600

    add checked boolean to item

--- src/main/kotlin/com/cbarnick/shoppinglist/model/Item.kt
+++ src/main/kotlin/com/cbarnick/shoppinglist/model/Item.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("ITEMS")
-data class Item(@Id var id: String?, val name: String, var quantity: Int = 1)
+data class Item(@Id var id: String?, val name: String, var quantity: Int = 1, var checked: Boolean = false)
